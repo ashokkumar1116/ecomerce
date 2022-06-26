10f94f1a4cd9b1868522f1532e85adda07bcad07
Sri Krishna <srikrishnak.dev@gmail.com>
2022-06-26 16:15:05 +1000

    Integrating stripe

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
+import {  Elements } from '@stripe/react-stripe-js'
 import { store, persistor } from './store/store';
 import App from './App';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import { PersistGate } from 'redux-persist/integration/react'
+import { stripePromise } from './utils/stripe/stripe.utils';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const entryPoint = document.getElementById("root");
+ReactDOM.render(
   // <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor} >
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
-    </Provider>
+    </Provider>,
   // </React.StrictMode>
-);
+  entryPoint
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
